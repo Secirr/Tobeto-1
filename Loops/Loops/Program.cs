399a7bda799a83fec94eb8e39db5dc56bbd5bbba
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-10-25 01:14:14 +0300

    adding While Loop to the Loops project

--- Loops/Loops/Program.cs
+++ Loops/Loops/Program.cs
@@ -1,42 +1,63 @@
-﻿
-//Number from 0 to 100
-//for (int i = 0; i <= 100; i++)
-//{
-//    Console.WriteLine(i);
-//}
+﻿//for
 
-//Even number from 0 to 100
-//for (int i = 0; i <= 100; i += 2)
-//{
-//    Console.WriteLine(i);
-//}
-
-//Odd number from 1 to 99
-//for (int i = 1; i <= 100; i += 2)
-//{
-//    Console.WriteLine(i);
-//}
-
-//Number from 100 to 0
-//for (int i = 100; i >= 0; i--)
-//{
-//    Console.WriteLine(i);
-//}
-
-////Even number from 100 to 0
-//for (int i = 100; i >= 0; i -= 2)
-//{
-//    Console.WriteLine(i);
-//}
-
-//Odd number from 99 to 1
-for (int i = 99; i >= 1; i -= 2)
+static void ForLoops()
 {
-    Console.WriteLine(i);
+
+    //Numbers from 0 to 100
+    //for (int i = 0; i <= 100; i++)
+    //{
+    //    Console.WriteLine(i);
+    //}
+
+    //Even numbers from 0 to 100
+    //for (int i = 0; i <= 100; i += 2)
+    //{
+    //    Console.WriteLine(i);
+    //}
+
+    //Odd numbers from 1 to 99
+    //for (int i = 1; i <= 100; i += 2)
+    //{
+    //    Console.WriteLine(i);
+    //}
+
+    //Numbers from 100 to 0
+    //for (int i = 100; i >= 0; i--)
+    //{
+    //    Console.WriteLine(i);
+    //}
+
+    //Even numbers from 100 to 0
+    //for (int i = 100; i >= 0; i -= 2)
+    //{
+    //    Console.WriteLine(i);
+    //}
+
+    //Odd numbers from 99 to 1
+    for (int i = 99; i >= 1; i -= 2)
+    {
+        Console.WriteLine(i);
+    }
+
+    Console.ReadKey();
+
 }
 
-Console.ReadKey();
+//While
+static void WhileLoop()
+{
+    int number = 50;
 
+    while (number >= 50)
+    {
+        Console.WriteLine(number);
+        number--;
+    }
 
+    Console.WriteLine("Now number is {0}", number);
+    Console.ReadKey();
+}
 
+//ForLoops();
+WhileLoop();
 

ecdfa403f8d9d771775db79c4fd292d7d52c5e84
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-10-24 22:25:39 +0300

    Adding the multidimensional arrays to the Arrays folder

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -19,13 +19,33 @@
 
 // Array | Concept three
 
-String[] customers = new String[4]{"Mina","Atilla","Sevim","Nebahat"};
+//String[] customers = new String[4]{"Mina","Atilla","Sevim","Nebahat"};
 
 
 
-foreach (String customer in customers)
+//foreach (String customer in customers)
+//{
+//    Console.WriteLine(customer);
+//}
+
+//Console.ReadKey();
+
+// Multidimensional Arrays
+
+String[,] multidimensional = new string[3, 2]
 {
-    Console.WriteLine(customer);
+    {"Iphone 13","Iphone 13 pro"},
+    {"Samsung s23","Samsung 23 Ultra"},
+    {"Xiaomi mi 13 lite","Xiaomi mi 13 pro"}
+};
+
+for (int i = 0; i <= multidimensional.GetUpperBound(0); i++)
+{
+    for (int j = 0; j <= multidimensional.GetUpperBound(1); j++)
+    {
+        Console.WriteLine(multidimensional[i,j]);
+    }
+    Console.WriteLine("************");
 }
 
 Console.ReadKey();

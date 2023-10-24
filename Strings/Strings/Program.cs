d3c4834f22cd4e3d36dd4c0238006b5d4399881d
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-10-25 02:03:00 +0300

    create Strigns folder and adding the Strings concepts

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+string isim = "Kadir";
+string soyisim = "Özdemir";
+
+foreach (var item in isim)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("{0} {1}", isim, soyisim);
+Console.WriteLine(String.Format("{0} {1}",isim,soyisim));
+
+Console.ReadKey();

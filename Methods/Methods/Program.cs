fbcf5275d9051373e134630cb67d89405bb3b949
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-10-24 16:16:53 +0300

    Adding the 'using ref keyword' to the 'Methots' project

--- Methods/Methods/Program.cs
+++ Methods/Methods/Program.cs
@@ -19,27 +19,38 @@
 
 
 
-    string employeeName = "kadir";
-    string employeeSurName = "özdemir";
-    float employeeSalary = calculatingSalary(25000,20);
+//string employeeName = "kadir";
+//string employeeSurName = "özdemir";
+//float employeeSalary = calculatingSalary(25000,20);
 
-    Console.WriteLine(
-        "Çalışan adı : {0} \n " +
-        "Çalışan Soyadı : {1} \n " +
-        "Çalışan maaşı : {2}"
-        ,employeeName,employeeSurName,employeeSalary);
-
- 
-
-    static float calculatingSalary(float salary , float tax = 18)
-    {
-         tax = (salary * tax) / 100;
-         salary = salary - tax;
-         return salary;
-    }
+//Console.WriteLine(
+//    "Çalışan adı : {0} \n " +
+//    "Çalışan Soyadı : {1} \n " +
+//    "Çalışan maaşı : {2}"
+//    ,employeeName,employeeSurName,employeeSalary);
 
 
 
+//static float calculatingSalary(float salary , float tax = 18)
+//{
+//     tax = (salary * tax) / 100;
+//     salary = salary - tax;
+//     return salary;
+//}
+
+
+int number1 = 20;
+int number2 = 100;
+var result = Add(ref number1, number2);
+
+Console.WriteLine(result);
+Console.WriteLine(number1);
 
 
 Console.ReadKey();
+
+static int Add(ref int number1, int number2)
+{
+    number1 = 30;
+    return number1 + number2;
+}

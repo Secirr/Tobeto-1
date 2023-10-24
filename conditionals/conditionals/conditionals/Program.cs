f746dc29d91956928dd1e971f1ac81580e45e1cd
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-10-24 14:17:45 +0300

    adding else if concept into conditionals folder

--- conditionals/conditionals/conditionals/Program.cs
+++ conditionals/conditionals/conditionals/Program.cs
@@ -2,13 +2,17 @@
 int sayi = 10;
 int sayi2 = 20;
 
-if(sayi == 10)
+if(sayi == 8)
 {
     Console.WriteLine("Sayı 10'dur");
 }
+else if(sayi == 15)
+{
+    Console.WriteLine("Sayı 15'dir");
+}
 else
 {
-    Console.WriteLine("Sayı 10 değildir.");
+    Console.WriteLine("Sayı 10 veya 20 değildir.");
 }
 
 Console.WriteLine(sayi2 == 15 ? "Sayı 20'dir" : "Sayı 20 değildir.");

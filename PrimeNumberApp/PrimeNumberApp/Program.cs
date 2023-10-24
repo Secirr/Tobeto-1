e3a4bfa5256ebfc101ae3ddc552731c57e6a860c
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-10-25 01:50:07 +0300

    create PrimeNumberApp folder and adding the IsPrimeNumber app

---
+++ PrimeNumberApp/PrimeNumberApp/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+static bool IsPrimeNumber(int number)
+{
+    bool result = true;
+
+    for (int i = 2; i < number-1 ; i++)
+    {
+        if (number%i==0)
+        {
+            result = false;
+            i = number;
+        }
+    }
+    return result;
+}
+
+if (IsPrimeNumber(6))
+{
+    Console.WriteLine("This is a prime number");
+}
+else
+{
+    Console.WriteLine("This is not a prime number");
+}
+
+Console.ReadKey();

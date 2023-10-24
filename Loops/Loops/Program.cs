f6adeffe38035c3101722fd3f2258040ee525152
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-10-25 01:10:00 +0300

    Create Loops folder and 'for loops' concepts

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,42 @@
+﻿
+//Number from 0 to 100
+//for (int i = 0; i <= 100; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+//Even number from 0 to 100
+//for (int i = 0; i <= 100; i += 2)
+//{
+//    Console.WriteLine(i);
+//}
+
+//Odd number from 1 to 99
+//for (int i = 1; i <= 100; i += 2)
+//{
+//    Console.WriteLine(i);
+//}
+
+//Number from 100 to 0
+//for (int i = 100; i >= 0; i--)
+//{
+//    Console.WriteLine(i);
+//}
+
+////Even number from 100 to 0
+//for (int i = 100; i >= 0; i -= 2)
+//{
+//    Console.WriteLine(i);
+//}
+
+//Odd number from 99 to 1
+for (int i = 99; i >= 1; i -= 2)
+{
+    Console.WriteLine(i);
+}
+
+Console.ReadKey();
+
+
+
+

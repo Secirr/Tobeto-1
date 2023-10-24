a49d2fe7fa01ba0186d362ed9b2e5d96659f12bf
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-10-24 14:12:06 +0300

    adding Single Line If concept into Conditionals folder

--- conditionals/conditionals/conditionals/Program.cs
+++ conditionals/conditionals/conditionals/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 int sayi = 10;
+int sayi2 = 20;
 
 if(sayi == 10)
 {
@@ -9,5 +10,7 @@ else
 {
     Console.WriteLine("Sayı 10 değildir.");
 }
+
+Console.WriteLine(sayi2 == 15 ? "Sayı 20'dir" : "Sayı 20 değildir.");
 
 Console.ReadKey();

3574c7a20bc0cf713da83bbd76d0814ec43c5c27
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-10-24 15:07:40 +0300

    Adding the addition methot to the Methots project

--- Methods/Methods/Program.cs
+++ Methods/Methods/Program.cs
@@ -1,9 +1,19 @@
 ﻿
 Except();
 
+int result = addition(15, 30);
+
+Console.WriteLine("Result : {0}", result);
+
 static void Except()
 {
     Console.WriteLine("this is except for methots");
+}
+
+
+static int addition(int number1, int number2){
+    int result = number1 + number2;
+    return result;
 }
 
 Console.ReadKey();

f86fe97c54d371a8095235f21743d4b38913933f
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-10-24 14:25:08 +0300

    adding switch-case concept into conditionals folder

--- conditionals/conditionals/conditionals/Program.cs
+++ conditionals/conditionals/conditionals/Program.cs
@@ -1,20 +1,38 @@
 ﻿
 int sayi = 10;
-int sayi2 = 20;
 
-if(sayi == 8)
-{
-    Console.WriteLine("Sayı 10'dur");
-}
-else if(sayi == 15)
-{
-    Console.WriteLine("Sayı 15'dir");
-}
-else
-{
-    Console.WriteLine("Sayı 10 veya 20 değildir.");
-}
+/*
+  
+    int sayi2 = 20;
 
-Console.WriteLine(sayi2 == 15 ? "Sayı 20'dir" : "Sayı 20 değildir.");
+    if(sayi == 8)
+    {
+        Console.WriteLine("Sayı 10'dur");
+    }
+    else if(sayi == 15)
+    {
+        Console.WriteLine("Sayı 15'dir");
+    }
+    else
+    {
+        Console.WriteLine("Sayı 10 veya 20 değildir.");
+    }
+
+    Console.WriteLine(sayi2 == 15 ? "Sayı 20'dir" : "Sayı 20 değildir.");
+
+*/
+
+switch (sayi)
+{
+    case 10:
+        Console.WriteLine("Sayı 10'dur");
+        break;
+    case 20:
+        Console.WriteLine("Sayı 20'dir.");
+        break;
+    default:
+        Console.WriteLine("sayı 10 ya da 20 değildir.");
+        break;
+}
 
 Console.ReadKey();

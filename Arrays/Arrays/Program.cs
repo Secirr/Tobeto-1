34fb1e47847523fcd939adbd02bfbb06dbeffad4
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-10-24 19:35:35 +0300

    Create Arrays folder and arrays concepts

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿
+// Array | concept one
+
+// String[] customers = new string[4];
+// customers[0] = "Mina";
+// customers[1] = "Atilla";
+// customers[2] = "Sevim";
+// customers[3] = "Nebahat";
+
+
+
+
+// Array | Concept two
+
+// String[] customers = { "Mina", "Atilla", "Sevim", "Nebahat"};
+
+
+
+
+// Array | Concept three
+
+String[] customers = new String[4]{"Mina","Atilla","Sevim","Nebahat"};
+
+
+
+foreach (String customer in customers)
+{
+    Console.WriteLine(customer);
+}
+
+Console.ReadKey();

dbcbceb1b7949ed11c3c91fea6564193ed4795ff
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-10-26 04:18:18 +0300

    Adding numerical lotto app and starting classes concepts

---
+++ Class/Class/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Class
+{
+    public class Customer
+    {
+
+        public int Id { get; set; }
+        public string? City { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+    }
+}
+
+
---
+++ NumaricalLotto-app/NumaricalLotto-app/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+
+
+//columnCount = ex: 8 --> up to 8. More than 8 is not allowed
+//1-49-->2,6,15,19,45,48 -->column -->all numbers are different
+//get column count from the user. ex : 3
+//play the game randomly-->use random class in c#
+//output ex:
+//2,6,15,18,45,48
+//4,6,18,19,43,49 //1,6,15,25,45,48
+//all columns must be the type of integer array
+
+int deger;
+Console.WriteLine("Bir değer giriniz: ");
+deger = Convert.ToInt32(Console.ReadLine());
+Random number = new Random();
+int[] random = new int[6];
+
+if (deger <= 8)
+{
+    for (int i = 0; i < deger; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            random[j] = number.Next(1, 49);
+
+        }
+        Array.Sort(random);
+
+        for (int k = 0; k < 6; k++)
+        {
+            Console.Write(random[k] + " ");
+        }
+
+        Console.WriteLine("\n");
+    }
+}
+
+else { Console.WriteLine("Hata!!!"); }
+
+
+
+Console.ReadKey();
+
+

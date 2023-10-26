ed6250346df31df1ce96ad8247fd03b90df19a11
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-10-27 02:05:45 +0300

    using the Interface Segregation(simulation4)

---
+++ InterfacesDemo/InterfacesDemo/Program.cs
@@ -0,0 +1,108 @@
+﻿
+namespace InterfacesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[3]
+            {
+                new Manager(),
+                new Worker(),
+                new Robot()
+            };
+
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+
+            IEat[] eats = new IEat[2]
+            {
+                new Manager(),
+                new Worker()
+            };
+
+            foreach (var eat in eats)
+            {
+                eat.Eat();
+            }
+
+            ISalary[] salaries = new ISalary[2]
+            {
+                new Manager(),
+                new Worker()
+            };
+
+            foreach (var salary in salaries)
+            {
+                salary.GetSalary();
+            }
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+    interface IWorker
+    {
+        void Work();
+    }
+
+    interface IEat
+    {
+        void Eat();
+    }
+
+    interface ISalary
+    {
+        void GetSalary();
+    }
+
+    class Manager : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Manager is eating");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Manager's salary day");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Manager is working");
+        }
+    }
+
+    class Worker : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Worker is eating");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Worker's salary day");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Worker is working");
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("Robot is working");
+        }
+    }
+
+}
+

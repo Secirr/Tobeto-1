5aca24da82aadcacd81a87bf2dc8feadcde60bc0
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-10-26 13:37:09 +0300

    Create Interfaces folder and use of the concept of interface

---
+++ InterFaces/InterFaces/Program.cs
@@ -0,0 +1,29 @@
+﻿
+//Interfaces concepts
+
+interface IPerson
+{
+    int Id { get; set; }
+    int FirtName { get; set; }
+    int LastName { get; set; }
+}
+
+class Customer:IPerson
+{
+    public int Id { get; set; }
+    public int FirtName { get; set; }
+    public int LastName { get; set; }
+
+    public string Address { get; set; }
+
+
+}
+
+class Student:IPerson
+{
+    public int Id { get; set; }
+    public int FirtName { get; set; }
+    public int LastName { get; set; }
+
+    public string Departmant { get; set; }
+}
